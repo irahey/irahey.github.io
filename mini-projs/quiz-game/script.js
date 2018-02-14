e988e9afa5131733680dc8fff6a36d1f898d9a0d
irahey <iraolivia.garcia@yahoo.com>
2018-02-14 10:46:59 +0000

    removed a few comment lines

--- mini-projs/quiz-game/script.js
+++ mini-projs/quiz-game/script.js
@@ -1,10 +1,3 @@
-/**
- * Created with JetBrains WebStorm.
- * User: pwanwu
- * Date: 18/09/2013
- * Time: 17:41
- * To change this template use File | Settings | File Templates.
- */
 
 var questions = [{
     question: "Mushrooms are part of the plant kingdom.",
